814dded410f17d4204ca0580be16eb9e9c1dba21
Chris Elliott <ctelliott@gmail.com>
2017-11-24 16:50:50 -0500

    Enabled ssl

--- docker/nginx-swarm/docker-entrypoint.sh
+++ docker/nginx-swarm/docker-entrypoint.sh
@@ -6,14 +6,14 @@ PROXY_AUTH_USER=${PROXY_AUTH_USER:-'admin'}
 sed -i "s/PROXY_DOMAIN/$PROXY_DOMAIN/g" /etc/nginx/conf.d/default.conf
 sed -i "s/SERVICE_NAME/$PROXY_SERVICE_NAME/g" /etc/nginx/nginx.conf
 
-# certbot certonly \
-#   --verbose \
-#   --noninteractive \
-#   --quiet \
-#   --standalone \
-#   --agree-tos \
-#   --email="${PROXY_EMAIL}" \
-#   --domains="${PROXY_DOMAIN}"
+certbot certonly \
+  --verbose \
+  --noninteractive \
+  --quiet \
+  --standalone \
+  --agree-tos \
+  --email="${PROXY_EMAIL}" \
+  --domains="${PROXY_DOMAIN}"
 
 mkdir -p /run/nginx
 
