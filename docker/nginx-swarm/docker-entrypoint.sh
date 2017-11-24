0e4be3e23eae21b6369697defb9fa75e5a49e866
Chris Elliott <ctelliott@gmail.com>
2017-11-24 17:18:51 -0500

    Rmoved certbot

--- docker/nginx-swarm/docker-entrypoint.sh
+++ docker/nginx-swarm/docker-entrypoint.sh
@@ -3,17 +3,7 @@ set -e
 
 PROXY_AUTH_USER=${PROXY_AUTH_USER:-'admin'}
 
-#sed -i "s/PROXY_DOMAIN/$PROXY_DOMAIN/g" /etc/nginx/conf.d/default.conf
 sed -i "s/SERVICE_NAME/$PROXY_SERVICE_NAME/g" /etc/nginx/nginx.conf
-
-# certbot certonly \
-#   --verbose \
-#   --noninteractive \
-#   --quiet \
-#   --standalone \
-#   --agree-tos \
-#   --email="${PROXY_EMAIL}" \
-#   --domains="${PROXY_DOMAIN}"
 
 mkdir -p /run/nginx
 
